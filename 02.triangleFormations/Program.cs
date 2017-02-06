9b06e15804133015e396c3d3ea8853edc3838b7e
pkemalov <petar.kemalov@gmail.com>
2017-02-06 20:33:42 +0200

    commited

---
+++ 02.triangleFormations/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+class TriangleFormations
+{
+    static void Main(string[] args)
+    {
+        var a = int.Parse(Console.ReadLine());
+        var b = int.Parse(Console.ReadLine());
+        var c = int.Parse(Console.ReadLine());
+
+        if (a > 0 && b > 0 && c > 0)
+        {
+            GetValidityConditions(a, b, c);
+            GetRightTriagleConditions(a, b, c);
+        }
+        else
+        {
+            Console.WriteLine("Invalid Triangle.");
+        }
+
+    }
+
+    static void GetRightTriagleConditions(int a, int b, int c)
+    {
+        var rightTriangleCondition1 = a * a + b * b == c * c;
+        var rightTriangleCondition2 = b * b + c * c == a * a;
+        var rightTriangleCondition3 = a * a + c * c == b * b;
+
+        if (rightTriangleCondition1)
+        {
+            Console.WriteLine("Triangle has a right angle between sides a and b");
+        }
+        else if (rightTriangleCondition2)
+        {
+            Console.WriteLine("Triangle has a right angle between sides b and c");
+        }
+        else if (rightTriangleCondition3)
+        {
+            Console.WriteLine("Triangle has a right angle between sides a and c");
+        }
+        else if (a + b > c && b + c > a && a + c > b)
+        {
+            Console.WriteLine("Triangle has no right angles");
+        }
+    }
+
+    static void GetValidityConditions(int a, int b, int c)
+    {
+        var triangleValidityCondition1 = a + b > c;
+        var triangleValidityCondition2 = b + c > a;
+        var triangleValidityCondition3 = a + c > b;
+
+        var ValidTriangle = triangleValidityCondition1 && triangleValidityCondition2 && triangleValidityCondition3;
+
+        if (!ValidTriangle)
+        {
+            Console.WriteLine("Invalid Triangle.");
+        }
+        else
+        {
+            Console.WriteLine("Triangle is valid.");
+        }
+    }
+}
---
+++ Debugging/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+class TrickyStrings
+{
+    static void Main(string[] args)
+    {
+        string delimiter = Console.ReadLine();
+        var numberOfStrings = int.Parse(Console.ReadLine());
+
+        var result = string.Empty;
+        var currentString = string.Empty;
+        GetFirstStrings(delimiter, numberOfStrings, ref result, ref currentString);
+        GetLastString(numberOfStrings, ref result, ref currentString);
+
+        Console.WriteLine(result);
+    }
+
+    static void GetLastString(int numberOfStrings, ref string result, ref string currentString)
+    {
+        for (int i = numberOfStrings - 1; i < numberOfStrings; i++)
+        {
+            currentString = Console.ReadLine();
+            result += currentString;
+        }
+    }
+
+    static void GetFirstStrings(string delimiter, int numberOfStrings, ref string result, ref string currentString)
+    {
+        for (int i = 0; i < numberOfStrings - 1; i++)
+        {
+            currentString = Console.ReadLine();
+            result += currentString + delimiter;
+        }
+    }
+}
